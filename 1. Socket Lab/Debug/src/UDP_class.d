b998806ee57b282d0bf2a4a157466bd1cc8d7a4e
Troy Dack <troy@dack.com.au>
2013-03-15 13:13:41 +1100

    Removed Debug files

--- 1. Socket Lab/Debug/src/UDP_class.d
+++
@@ -1,3 +0,0 @@
-src/UDP_class.d: ../src/UDP_class.cpp ../src/UDP_class.h
-
-../src/UDP_class.h:
--- 1. Socket Lab/Debug/src/udp_alarm_proforma.d
+++
@@ -1,4 +0,0 @@
-src/udp_alarm_proforma.d: ../src/udp_alarm_proforma.cpp \
- ../src/UDP_class.h
-
-../src/UDP_class.h:
